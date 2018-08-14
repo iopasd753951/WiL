14df55d033075e4dad3ec27a07594eaeafe82285
IOPASD753951\DSM2018 <iopasd753951@gmail.com>
2018-08-14 12:49:30 +0900

    조금 배운 php크롤링

---
+++ phpuser/index.php
@@ -0,0 +1,13 @@
+<?php
+	import requests	
+	from bs4 import BeautifulSoup
+
+	res = requests.get('http://v.media.daum.net/v/20170615203441266')
+
+	soup = BeautifulSoup(res.content, 'html.parser')
+
+	title = soup.fin('title')
+
+	print(title.get_text())
+
+?>
