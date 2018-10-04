6e6a553095c39dae25d1a0a16310b37c95379a15
IOPASD753951\DSM2018 <iopasd753951@gmail.com>
2018-10-04 23:36:40 +0900

    Flask_Sessions

--- 012_Flask.py
+++ 012_Flask.py
@@ -4,16 +4,28 @@
 # session은 클라이언트가 서버에 로그인하고 로그아웃하기까지의 시간 간격입니다.
 # session통해 저장해야하는 데이터는 서버의 임시 디렉토리에 저장된다.
 
-# session은 ["세션 변수" : 키 값]으로 구성된 dictionary 개체입니다.
+# session은 ["세션 변수" : 키 값]으로 구성된 dictionary 개체이다.
 import username as username
 from flask import Flask, redirect, request, session, url_for
 app = Flask(__name__)
 
-session[username]='admin'
+# session[username] = 'admin'
 # username을 "세션 변수"로 설정함
 
-session.pop('name', None)
-# 세션변수를 해제 --> pop()으로 해제해줌ㅎ
+# session.pop('name', None)
+# 세션변수를 해제 --> pop()으로 해제해줌
+
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+
+        return 'Logged in as ' + username + '<br>' + \
+               '<b><a href = \'/logout\'>click here to log out</a></b>'
+
+    return 'You are not logged in <br><a href = \'/login\'></b>' + \
+           "click here to log in</b></a>"
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -23,9 +35,20 @@ def login():
         return redirect(url_for('index'))
     return '''
 
-   <form action = "" method = "post">
+  <form action = "" method = "post">
       <p><input type = text name = username/></p>
-      <p<<input type = submit value = Login/></p>
-   </form>
+      <p><input type = submit value = Login/></p>
+   </form>'
 
    '''
+
+
+@app.route('/logout')
+def logout():
+   # 세션에 사용자 이름이 있을 경우 삭제함.
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
